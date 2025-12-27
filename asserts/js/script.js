10c79364b8f39ac89bb01b44d6d132fc67d9a051
webtechky <webtechky4@gmail.com>
2025-12-27 11:38:39 +0530

    Add files via upload

---
+++ asserts/js/script.js
@@ -0,0 +1,24 @@
+const navLinks = document.getElementById('navLinks');
+const hamBurger = document.getElementById('hamburger-icon');
+const navItems = document.querySelectorAll('.nav_links li a');
+const sections = document.querySelectorAll('section');
+
+hamBurger.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+});
+
+const isElementInViewport = (el) => {
+    const rect = el.getBoundingClientRect();
+    return rect.top <= 150 && rect.bottom >= 150;
+};
+
+window.addEventListener('scroll', () => {
+    sections.forEach(section => {
+        const id = section.getAttribute('id');
+        const link = document.querySelector(`.nav_links li a[href="#${id}"]`);
+        if (isElementInViewport(section)) {
+            navItems.forEach(item => item.classList.remove('active'));
+            if (link) link.classList.add('active');
+        }
+    });
+});
